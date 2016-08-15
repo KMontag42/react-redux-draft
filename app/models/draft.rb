c0939fa6934cc8854c9a2986bbe35a8d6c1e01a0
kylemontag <kyle@ditto.us.com>
2016-08-15 17:49:00 -0400

    add some basic draft models

--- app/models/draft.rb
+++ app/models/draft.rb
@@ -1,2 +1,15 @@
 class Draft < ApplicationRecord
+
+  def users
+    state['participatingUsers']
+  end
+
+  def picks
+    state['picks']
+  end
+
+  def picks_for_round(round)
+    state['picks'].select {|x| x['round'] == round}
+  end
+
 end
